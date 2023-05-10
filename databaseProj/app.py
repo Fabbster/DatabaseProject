a84eefac69d547d3d765e4c3f6ac4fcac13cb031
Hassan Kassar <71495793+hassank1@users.noreply.github.com>
2023-05-10 10:26:01 +0200

    sql connector implemented

--- databaseProj/app.py
+++ databaseProj/app.py
@@ -1,38 +1,31 @@
 from flask import Flask
-# from config import Config
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 from flask import Flask, render_template
-import os
 
 MainPage = Flask(__name__)
 
-@MainPage.route('/')
-def index():
-    title = 'Flask Website'
-    app_name = 'My Flask App'
-    return render_template('index.html', title=title, app_name=app_name)
+# @MainPage.route('/')
+# def index():
+#     title = 'Flask Website'
+#     app_name = 'My Flask App'
+#     return render_template('index.html', title=title, app_name=app_name)
 
 app = Flask(__name__)
-app.config.from_object(Config())
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost:3306/mydatabase'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.name
 
 @app.route('/')
 def index():
-    user = User('John Doe', 'john@example.com')
-    db.session.add(user)
-    db.session.commit()
-    return 'User added'
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='test_user',
+        password='Test123456!',
+        database='recepie_finder'
+    )
+    
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    results = cursor.fetchall()
 
+    return str(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
