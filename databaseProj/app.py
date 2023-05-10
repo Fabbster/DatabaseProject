cf643179d746d53cff45c7f8c38da52408bff8c8
Fabian Zander <fabianzander2@gmail.com>
2023-05-10 09:09:50 +0200

    foimefoiesj

---
+++ databaseProj/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+import os
+
+MainPage = Flask(__name__)
+
+@MainPage.route('/')
+def index():
+    title = 'Flask Website'
+    app_name = 'My Flask App'
+    return render_template('index.html', title=title, app_name=app_name)
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+@app.route('/')
+def index():
+    user = User('John Doe', 'john@example.com')
+    db.session.add(user)
+    db.session.commit()
+    return 'User added'
+
